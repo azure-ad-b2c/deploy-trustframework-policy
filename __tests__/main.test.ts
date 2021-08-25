7463042122ee3c2c62f9c0b91549559371289d40
Yoel Hor <yoelhor@live.com>
2021-08-25 10:53:08 +0300

    v2.0

---
+++ __tests__/main.test.ts
@@ -0,0 +1,23 @@
+
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
+import { expect, test } from '@jest/globals'
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', async () => {
+  
+
+})
+
+/*
+test('wait 500 ms', async () => {
+  const start = new Date()
+  await wait(500)
+  const end = new Date()
+  var delta = Math.abs(end.getTime() - start.getTime())
+  expect(delta).toBeGreaterThan(450)
+})
+*/
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 const core = require('@actions/core');
 const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
 (global as any).fetch = require('node-fetch'); // Polyfill for graph client
 import { Client } from '@microsoft/microsoft-graph-client';
 import { ClientCredentialsAuthProvider } from './auth';
 
-async function main() {
+async function run(): Promise<void> {
   try {
-    const file = core.getInput('file');
-    const policy = core.getInput('policy');
+    const folder = core.getInput('folder');
+    const files = core.getInput('files');
     const tenant = core.getInput('tenant');
     const clientId = core.getInput('clientId');
     const clientSecret = core.getInput('clientSecret');
+
+
 
     let client = Client.initWithMiddleware({
       authProvider: new ClientCredentialsAuthProvider(
@@ -21,17 +25,65 @@ async function main() {
       defaultVersion: 'beta'
     });
 
-    let fileStream = fs.createReadStream(file);
-    let response = await client
-      .api(`trustFramework/policies/${policy}/$value`)
-      .putStream(fileStream);
+    // Create an array of policy files
+    let filesArray = files.split(",");
 
-    core.info('Wrote policy using Microsoft Graph');
+    for (let f of filesArray) {
+
+      const file: string = path.join(folder, f.trim());
+
+      if (file.length > 0 && fs.existsSync(file)) {
+
+        core.info('Uploading policy file ' + file + ' ...');
+
+        // Get the policy name
+        let policyName: string = '';
+        const data = await fsPromises.readFile(file);
+
+        let result = data.toString().match(/(?<=\bPolicyId=")[^"]*/gm);
+
+        if (result && result.length > 0)
+          policyName = result[0];
+
+        // Upload the policy
+        let fileStream = fs.createReadStream(file);
+        let response = await client
+          .api(`trustFramework/policies/${policyName}/$value`)
+          .putStream(fileStream);
+
+        core.info('Uploading policy file ' + file + ' task is completed.');
+      }
+      else {
+        core.warning('Policy file ' + file + ' not found.')
+      }
+    }
+
   } catch (error) {
     let errorText = error.message ?? error;
-    core.error('Action failed: ' + errorText);
-    core.setFailed();
+    core.setFailed(errorText);
   }
 }
 
-main();
+run();
+
+
+
+// import * as core from '@actions/core'
+// import {wait} from './wait'
+
+// async function run(): Promise<void> {
+//   try {
+//     const ms: string = core.getInput('milliseconds')
+//     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+
+//     core.debug(new Date().toTimeString())
+//     await wait(parseInt(ms, 10))
+//     core.debug(new Date().toTimeString())
+
+//     core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     core.setFailed(error.message)
+//   }
+// }
+
+// run()
